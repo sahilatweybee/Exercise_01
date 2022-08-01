486faf27de11480cdf540073d99c23eea71b909a
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-08-01 17:06:49 +0530

    Add project files.

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A
+            // ABA
+            // ABCBA
+            // ABCDCBA
+            // ABCDEDCBA
+
+            for (int i = 0; i <= 5; i++)
+            {
+                int str = 65;
+                for (int j = 1; j < 2 * i; j++)
+                {
+                    if (j < i)
+                    {
+                        Console.Write(Convert.ToChar(str) + " ");
+                        str++;
+                    }
+                    else
+                    {
+                        Console.Write(Convert.ToChar(str) + " ");
+                        str--;
+                    }
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
